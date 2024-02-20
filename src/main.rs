efd92cdcb999294eaa6e2cd2e731a89f1342ab48
Jing Mai <jingmai@pku.edu.cn>
2024-02-20 21:32:27 +0800

    modularization & error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use minigrep::Config;
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("query: {}", config.query);
+    println!("file_path: {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
