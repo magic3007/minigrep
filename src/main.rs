2c0e3dec0bd83ac1550a378482117657a8546629
Jing Mai <jingmai@pku.edu.cn>
2024-02-20 22:57:18 +0800

    TDD

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("query: {}", config.query);
-    println!("file_path: {}", config.file_path);
-
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
